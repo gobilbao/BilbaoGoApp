bbf4863044de64839220d5b9a2503cd4bd404120
Ainhoa <ainhoacaro@outlook.com>
2019-05-13 20:40:14 +0200

    A.C.G 13052019

---
+++ Vuforia/Assets/Scripts/GetDatos.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        WWW ruta = new WWW ("https://bilbaogo-2c61c.firebaseio.com/.json");
+        yield return ruta;
+        string datos = ruta.    
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
